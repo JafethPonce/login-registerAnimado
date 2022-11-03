4c2cc1900168c63a6d22c9952f857eaa54a98ec0
Jafeth Ponce <115910638+JafethPonce@users.noreply.github.com>
2022-11-02 22:33:07 -0600

    Add files via upload

---
+++ Login-animado/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Animado</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="box">
+		<form autocomplete="off">
+			<h2>Inicia sesión</h2>
+			<div class="inputBox">
+				<input type="text">
+				<span>Usuario</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="password">
+				<span>Contraseña</span>
+				<i></i>
+			</div>
+			<div class="links">
+				<a href="#">¿Olvidaste la contraseña?</a>
+				<a href="registro.php">Registrate</a>
+			</div>
+			<input type="submit" value="Iniciar">
+		</form>
+	</div>
+</body>
+</html>
---
+++ Login-animado/registro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+	<link rel="stylesheet" href="register.css">
+</head>
+<body>
+	<div class="box">
+		<form autocomplete="off">
+			<h2>Registrate</h2>
+            <div class="inputBox">
+				<input type="text">
+				<span>Nombres</span>
+				<i></i>
+			</div>
+            <div class="inputBox">
+				<input type="text">
+				<span>Apellidos</span>
+				<i></i>
+			</div>
+            <div class="inputBox">
+				<input type="text">
+				<span>Celular</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="text">
+				<span>Usuario</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="password">
+				<span>Contraseña</span>
+				<i></i>
+			</div>
+			<div class="links">
+				<a href="login.php">Inicia sesión</a>
+			</div>
+			<input type="submit" value="Registrar">
+		</form>
+	</div>
+</body>
+</html>
